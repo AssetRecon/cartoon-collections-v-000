e8e20f882282b61225323812508adc9deec175f9
Edsel Caprice <caprice@mail.usf.edu>
2018-06-20 01:25:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,12 @@ if array.find { |x| x.length > 4 }.class == String
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+  array.find do |x|
+    cheese_types.include?(x)
+
+    end
+  end
